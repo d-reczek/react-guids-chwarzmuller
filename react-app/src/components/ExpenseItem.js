f4ad43375ca52192da20d2a62b59af6afa6f6304
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-07-12 21:29:58 +0200

    add app

---
+++ react-app/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import "./ExpenseItem.css";
+
+const ExpenseItem = () => {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">$234</div>
+      </div>
+    </div>
+  );
+};
+export default ExpenseItem;
