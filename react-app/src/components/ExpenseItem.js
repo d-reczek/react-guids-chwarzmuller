08aa35673622720b6ac48ec0cf9aa7f1e11c1eac
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-07-13 18:47:28 +0200

    add props

--- react-app/src/components/ExpenseItem.js
+++ react-app/src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expnseDate = new Date(2021, 2, 22);
-  const expenseTitle = "Car insruance";
-  const expenseAmount = 2137;
+const ExpenseItem = ({ title, amount, date }) => {
   return (
     <div className="expense-item">
-      <div>{expnseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
