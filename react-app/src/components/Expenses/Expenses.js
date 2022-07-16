6efb73326fabeefcf6822a6610f04db59048cc34
Dariusz Reczek <dariusz.reczek@gmail.com>
2022-07-16 18:48:56 +0200

    lifting state up

--- react-app/src/components/Expenses/Expenses.js
+++ react-app/src/components/Expenses/Expenses.js
@@ -1,31 +1,41 @@
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import { useState } from "react";
 
 const Expenses = ({ items }) => {
+  const [yearValue, setYearValue] = useState(null);
+  const onSelecetDateValue = year => {
+    setYearValue(year);
+    console.log(yearValue);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
-    </Card>
+    <>
+      <Card className="expenses">
+        <ExpensesFilter saveYearValue={onSelecetDateValue} />
+        <ExpenseItem
+          title={items[0].title}
+          amount={items[0].amount}
+          date={items[0].date}
+        />
+        <ExpenseItem
+          title={items[1].title}
+          amount={items[1].amount}
+          date={items[1].date}
+        />
+        <ExpenseItem
+          title={items[2].title}
+          amount={items[2].amount}
+          date={items[2].date}
+        />
+        <ExpenseItem
+          title={items[3].title}
+          amount={items[3].amount}
+          date={items[3].date}
+        />
+      </Card>
+    </>
   );
 };
 
